e3f95a2976c6a2a6dd54bafb444f8dca03140968
CoffeeDuck <davidlive0159@gmail.com>
2020-11-19 00:35:35 -0500

    fix messsage

--- app/Views/layouts/public.php
+++ app/Views/layouts/public.php
@@ -14,8 +14,8 @@
 	<meta property="og:type" content="article" />
 	<meta property="og:url" content="<?= base_url() ?>" />
 	<meta property="og:image" content="<?= base_url() ?>/images/meta.png" />
-	<meta property="og:description" content="Se bienvenido a nuestra comundidad de artistas y dibujantes Art's Book" />
-	<meta property="og:description" content="Se bienvenido a nuestra comundidad de artistas y dibujantes Art's Book 😁" />
+	<meta property="og:description" content="Se bienvenid@ a nuestra comunidad de artistas y dibujantes Art's Book" />
+	<meta property="og:description" content="Se bienvenid@ a nuestra comunidad de artistas y dibujantes Art's Book 😁" />
 	<style media="screen">
 
 		#app-body{
