abf802aa1b2d8f46ad78022f6c91defb69f0f067
CoffeeDuck <davidlive0159@gmail.com>
2020-11-21 19:58:31 -0500

    update change resources css an js

--- app/Views/layouts_parts/header.php
+++ app/Views/layouts_parts/header.php
@@ -1,4 +1,4 @@
-<?php $version = 0.012 ?>
+<?php $version = 0.013 ?>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <title>ARTS BOOK</title>
