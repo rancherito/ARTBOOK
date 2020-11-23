169d4eccd774fe2fca7d7153f94b65a33634c698
CoffeeDuck <davidlive0159@gmail.com>
2020-11-23 08:27:43 -0500

    dfsd

--- app/Views/layouts_parts/header.php
+++ app/Views/layouts_parts/header.php
@@ -1,4 +1,4 @@
-<?php $version = 'beta_0.014' ?>
+<?php $version = 'beta_0.015' ?>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <title>ARTS BOOK</title>
