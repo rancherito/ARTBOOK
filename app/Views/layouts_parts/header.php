b1853a5e6e1f4413f8924d9fd8e896fe2c3af97f
CoffeeDuck <davidlive0159@gmail.com>
2020-12-03 13:52:22 -0500

    update styles

--- app/Views/layouts_parts/header.php
+++ app/Views/layouts_parts/header.php
@@ -1,4 +1,4 @@
-<?php $version = 'beta_0.03' ?>
+<?php $version = 'beta_0.031' ?>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <title>ARTS BOOK</title>
