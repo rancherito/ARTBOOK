a2993f8bde9a2096abc87d008ca362c79c97bec3
rancherito <davidlive0159@gmail.com>
2020-12-18 17:34:50 -0500

    composer install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'artsbook/appproject',
+  ),
+  'versions' => 
+  array (
+    'artsbook/appproject' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'facebook/graph-sdk' => 
+    array (
+      'pretty_version' => '5.x-dev',
+      'version' => '5.9999999.9999999.9999999-dev',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '2d8250638b33d73e7a87add65f47fabf91f8ad9b',
+    ),
+  ),
+);
