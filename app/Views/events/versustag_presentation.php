9e066ba5c11e295ce5a9ff8670034691a7211870
rancherito <davidlive0159@gmail.com>
2020-12-29 04:13:45 -0500

    sdfghjkl

--- app/Views/events/versustag_presentation.php
+++ app/Views/events/versustag_presentation.php
@@ -6,20 +6,25 @@
 	.versus-list{
 		display: flex;
 	}
+	.versus-list-reverse{
+		flex-direction: row-reverse;
+	}
 	.versus-list > div{
 		width: 50%;
 	}
 </style>
 <?php template_start() ?>
 	<div class="">
+		<?php $count = 0 ?>
 		<?php foreach ($versus_list as $key => $versus): ?>
-			<div class="versus-list">
+			<?php $count++ ?>
+			<div class="versus-list <?= $count%2 == 0 ? 'versus-list-reverse' : '' ?>">
 				<div class="versus-list-winner">
 					GANADOR(ES) <br>
 					<?php foreach ($versus as $key => $winner): ?>
 						<?php if ($winner['is_winner'] == 1): ?>
 
-							<img src="<?= base_url()."/images/artworks_lite/$winner[artwork].$winner[extension]" ?>" alt="artwork">
+							<img style="width: 260px" src="<?= base_url()."/images/artworks_lite/$winner[artwork].$winner[extension]" ?>" alt="artwork">
 
 							<br>
 						<?php endif; ?>
@@ -28,7 +33,7 @@
 				<div class="versus-list-info">
 					OTRAS PARTICIPACIONES <br>
 					<?php foreach ($versus as $key => $others): ?>
-						<img style="width: 200px" src="<?= base_url()."/images/artworks_lite/$others[artwork].$others[extension]" ?>" alt="artwork">
+						<img style="width: 100px" src="<?= base_url()."/images/artworks_lite/$others[artwork].$others[extension]" ?>" alt="artwork">
 					<?php endforeach; ?>
 				</div>
 			</div>
