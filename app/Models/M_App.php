28b727916f30813e810b5e0d22e65e7d22415fbf
rancherito <davidlive0159@gmail.com>
2021-01-10 00:07:01 -0500

    LIKE

--- app/Models/M_App.php
+++ app/Models/M_App.php
@@ -16,21 +16,13 @@ class M_App
 
 	public static function qry_images_new_list($current_account = '$$')
     {
-    	$sql = "SELECT TOP 16 i.id_image, i.[description], i.accessname, i.extension, i.height, i.width, i.uploaded_date, i.name,
-		u.nickname, u.account, category_main, SUBSTRING(master.dbo.fn_varbintohexstr(HashBytes('MD5', u.[user])), 3, 32) user_avatar,
-		ISNULL((SELECT heart FROM app.tb_artwork_favorites f, users.tb_users uu  WHERE uu.account = ? AND f.id_artwork = i.id_image AND uu.id_user = f.id_user), 0) heart
-		FROM app.tb_images i, [users].tb_users u
-		WHERE i.autor = u.id_user AND i.[state] = 'A' ORDER BY i.uploaded_date DESC;";
+    	$sql = "EXEC app.sp_artwoks_list @account = ?, @type = 'NEW'";
 
 		return query_database($sql, [$current_account]);
     }
     public static function qry_images_list($current_account = '$$')
     {
-    	$sql = "SELECT TOP 30 i.id_image, i.[description], i.accessname, i.extension, i.height, i.width, i.uploaded_date, i.name,
-		u.nickname, u.account, category_main, SUBSTRING(master.dbo.fn_varbintohexstr(HashBytes('MD5', u.[user])), 3, 32) user_avatar,
-		ISNULL((SELECT heart FROM app.tb_artwork_favorites f, users.tb_users uu  WHERE uu.account = ? AND f.id_artwork = i.id_image AND uu.id_user = f.id_user), 0) heart
-		FROM app.tb_images i, [users].tb_users u
-		WHERE i.autor = u.id_user AND i.[state] = 'A' ORDER BY NEWID();";
+    	$sql = "EXEC app.sp_artwoks_list @account = ?, @type = 'TOP'";
 
 		return query_database($sql,[$current_account]);
     }
