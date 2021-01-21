bf41224d4b3fab588e9bb369775fea5d55cf4176
rancherito <davidlive0159@gmail.com>
2021-01-21 06:17:56 -0500

    dfghjk

--- app/Views/layouts_parts/header.php
+++ app/Views/layouts_parts/header.php
@@ -30,17 +30,13 @@ $GLOBALS['version'] = $version;
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link rel="icon" type="image/png" href="<?= base_url() ?>/images/icon.png">
 <link rel="stylesheet" href="<?= base_url() ?>/font/RobotoCondensed/RobotoCondensed.css">
-<link rel="stylesheet" href="<?= base_url() ?>/font/mdi/css/materialdesignicons.min.css">
 
 <link rel="stylesheet" href="<?= base_url() ?>/libs/materialize/css/materialize.min.css">
 <link rel="stylesheet" href="<?= base_url() ?>/css/helpers.css?v=<?= $version ?>">
-<link rel="stylesheet" href="<?= base_url() ?>/libs/animate/animate.min.css">
+
 <link rel="stylesheet" href="<?= base_url() ?>/css/custom_materialize.css?v=<?= $version ?>">
 <script src="<?= base_url() ?>/js/script.js?v=<?= $version ?>"></script>
 <script src="<?= base_url() ?>/js/jquery-3.4.1.min.js"></script>
-<script src="<?= base_url() ?>/libs/materialize/js/materialize.min.js"></script>
-
-
 
 
 <link rel="stylesheet" href="<?= base_url() ?>/libs/cgVue/cg.components.vue.css?v=<?= $version ?>">
